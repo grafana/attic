f25c789e9ff256892647df57fb06b3df1c48e444
Chris Christensen <cchristensen@llnw.com>
2015-03-28 23:14:22 -0500

    Expose item id UI lookup and parsing logic.
    Noteworthy that the request is fairly efficient since each group of id requests
    is performed in a single query to the server.

--- datasources/zabbix/zabbix.js
+++ datasources/zabbix/zabbix.js
@@ -27,44 +27,61 @@ function (angular, _, kbn) {
       // get from & to in seconds
       var from = kbn.parseDate(options.range.from).getTime();
       var to = kbn.parseDate(options.range.to).getTime();
-      var queries = ""; // Derive from: options.targets, look at: convertTargetToQuery
+      var items = _.pluck(options.targets, 'itemid');
 
       from = Math.ceil(from/1000);
       to = Math.ceil(to/1000);
 
-      return this.performTimeSeriesQuery(queries, from, to)
+      return this.performTimeSeriesQuery(items, from, to)
         .then(_.bind(function (response) {
-          return {data: [{datapoints: _.map(response.data.result, function (p) { return [p.value, p.clock*1000];}), target: "I1"}]};
+
+          // Response should be in the format:
+          //[{
+          //  target: "Metric name",
+          //  datapoints: [[<value>, <unixtime>], ...]
+          //},]
+
+          return {
+            data: _.map(
+              // Index returned datapoints by item/metric id
+              _.groupBy(response.data.result, function (p) { return p.itemid }),
+                // Foreach itemid index: iterate over the data points and
+                //  normalize to Grafana response format.
+                function (i, id) {
+                  return {
+                    // TODO unroll return with itemid:alias map
+                    target: id,
+                    datapoints: _.map(i, function (p) { return [p.value, p.clock*1000];})
+                  };
+              })
+          };
         },options));
     };
 
-    ZabbixAPIDatasource.prototype.performTimeSeriesQuery = function(queries, start, end) {
-      var reqBody = {
-        jsonrpc: '2.0',
-        method: 'history.get',
-        params: {
-            output: 'extend',
-            history: 0,
-            itemids: 683,
-            sortfield: 'clock',
-            sortorder: 'DESC',
-            limit: 1000,
-            time_from: start,
-        },
-        auth: this.auth,
-        id: 1
-      };
-
-      // Relative queries (e.g. last hour) don't include an end time
-      if (end) {
-        reqBody.params.time_till = end;
-      }
-
+    ZabbixAPIDatasource.prototype.performTimeSeriesQuery = function(items, start, end) {
       var options = {
         method: 'POST',
         url: this.url + '',
-        data: reqBody
+        data: {
+          jsonrpc: '2.0',
+          method: 'history.get',
+          params: {
+              output: 'extend',
+              history: 0, // TODO this needs to be exposed
+              itemids: items,
+              sortfield: 'clock',
+              sortorder: 'DESC',
+              limit: 1000, // Where do he defaults for this come from?
+              time_from: start,
+          },
+          auth: this.auth,
+          id: 1
+        },
       };
+      // Relative queries (e.g. last hour) don't include an end time
+      if (end) {
+        options.data.params.time_till = end;
+      }
 
       return $http(options);
     };
