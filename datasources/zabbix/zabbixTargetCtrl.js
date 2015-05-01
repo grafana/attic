5f027b4b2cbf7f1dca4f9635c0fbde6b25e94d1c
Alexander Zobnin <alexanderzobnin@gmail.com>
2015-05-01 17:35:05 +0300

    Add host group selection.

--- datasources/zabbix/zabbixTargetCtrl.js
+++ datasources/zabbix/zabbixTargetCtrl.js
@@ -12,10 +12,12 @@ function (angular, _) {
     $scope.init = function() {
       $scope.targetLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
       $scope.metric = {
-        hostlist: ["Loading..."],
-        itemlist: ["Loading..."]
+        hostGroupList: ["Loading..."],
+        hostList: ["Loading..."],
+        itemList: ["Loading..."]
       };
 
+      $scope.updateHostGroupList();
       $scope.updateHostList();
       if ($scope.target.host.hostid) {
         $scope.updateItemList($scope.target.host.hostid);
@@ -32,6 +34,20 @@ function (angular, _) {
       }
     };
 
+    // Call when host group selected
+    $scope.selectHostGroup = function() {
+      // Update item list
+      if ($scope.target.hostGroup) {
+        $scope.updateHostList($scope.target.hostGroup.groupid);
+      } else {
+        $scope.updateHostList('');
+      }
+      $scope.target.errors = validateTarget($scope.target);
+      if (!_.isEqual($scope.oldTarget, $scope.target) && _.isEmpty($scope.target.errors)) {
+        $scope.oldTarget = angular.copy($scope.target);
+        $scope.get_data();
+      }
+    };
 
     // Call when host selected
     $scope.selectHost = function() {
@@ -70,13 +86,26 @@ function (angular, _) {
     //////////////////////////////
 
     // Update list of hosts
-    $scope.updateHostList = function() {
-      $scope.datasource.performHostSuggestQuery().then(function (series) {
-        $scope.metric.hostlist = series;
-        $scope.target.host = $scope.metric.hostlist.filter(function (item, index, array) {
-            // Find selected host in metric.hostlist
-            return (item.hostid == $scope.target.host.hostid);
+    $scope.updateHostGroupList = function() {
+      $scope.datasource.performHostGroupSuggestQuery().then(function (series) {
+        $scope.metric.hostGroupList = series;
+        if ($scope.target.hostGroup) {
+          $scope.target.hostGroup = $scope.metric.hostGroupList.filter(function (item, index, array) {
+            // Find selected host in metric.hostList
+            return (item.groupid == $scope.target.hostGroup.groupid);
           }).pop();
+        }
+      });
+    };
+
+    // Update list of hosts
+    $scope.updateHostList = function(groupid) {
+      $scope.datasource.performHostSuggestQuery(groupid).then(function (series) {
+        $scope.metric.hostList = series;
+        $scope.target.host = $scope.metric.hostList.filter(function (item, index, array) {
+          // Find selected host in metric.hostList
+          return (item.hostid == $scope.target.host.hostid);
+        }).pop();
       });
     };
 
@@ -85,9 +114,9 @@ function (angular, _) {
       // Update only if host selected
       if (hostid) {
         $scope.datasource.performItemSuggestQuery(hostid).then(function (series) {
-          $scope.metric.itemlist = series;
-          $scope.target.item = $scope.metric.itemlist.filter(function (item, index, array) {
-            // Find selected item in metric.hostlist
+          $scope.metric.itemList = series;
+          $scope.target.item = $scope.metric.itemList.filter(function (item, index, array) {
+            // Find selected item in metric.hostList
             return (item.itemid == $scope.target.item.itemid);
           }).pop();
         });
